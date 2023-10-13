ffe0ab26c681a34e1d1f38d535fc414fb370ad23
isado <isadorabellaguarda@gmail.com>
2023-10-13 11:47:37 -0300

    first commit

---
+++ src/main/java/com/isadorastrottmann/apicrudcustomer/ApiCrudCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.isadorastrottmann.apicrudcustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCrudCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCrudCustomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isadorastrottmann/apicrudcustomer/ApiCrudCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isadorastrottmann.apicrudcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCrudCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
