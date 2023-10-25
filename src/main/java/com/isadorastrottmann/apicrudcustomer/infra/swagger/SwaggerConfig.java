9547bb2c9ec2b79a22f5dd18fc4a9fba1294f83d
isado <isadorabellaguarda@gmail.com>
2023-10-25 08:35:00 -0300

    doc: adding security scheme to documentation

--- src/main/java/com/isadorastrottmann/apicrudcustomer/infra/swagger/SwaggerConfig.java
+++ src/main/java/com/isadorastrottmann/apicrudcustomer/infra/swagger/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.isadorastrottmann.apicrudcustomer.infra.swagger;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +12,18 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openApi() {
-        return new OpenAPI().info(getInfo());
+        return new OpenAPI()
+                .components(getComponents())
+                .info(getInfo());
+    }
+
+    private Components getComponents(){
+        return new Components()
+                .addSecuritySchemes("bearer-key",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"));
     }
 
     private Info getInfo() {
@@ -19,5 +32,4 @@ public class SwaggerConfig {
                 .version("1.0.0")
                 .description("API with CRUD operations for a Customer entity");
     }
-
 }
