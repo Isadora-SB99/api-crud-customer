504699eaae6e152417f3c0568beb864cfa63f915
isado <isadorabellaguarda@gmail.com>
2023-11-07 13:21:42 -0300

    test: testing token service

--- src/test/java/com/isadorastrottmann/apicrudcustomer/infra/security/TokenServiceTest.java
+++ src/test/java/com/isadorastrottmann/apicrudcustomer/infra/security/TokenServiceTest.java
@@ -2,20 +2,16 @@ package com.isadorastrottmann.apicrudcustomer.infra.security;
 
 import com.isadorastrottmann.apicrudcustomer.stubs.CustomerStub;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest //cria o contexto da aplicação spring e injeta as dependencias
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
 class TokenServiceTest {
-//    erro de "secret cannot be null"...
+
     @Autowired
     private TokenService tokenService;
-
-//    public TokenServiceTest(TokenService tokenService) {
-//        this.tokenService = tokenService;
-//    }
 
     @Test
     void getSubject() {
