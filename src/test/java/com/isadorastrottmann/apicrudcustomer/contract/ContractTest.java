e03d376c08c2b8d25d691ac17998412741aa69f6
isado <isadorabellaguarda@gmail.com>
2023-11-09 08:07:49 -0300

    comentando imports da ContractTest

--- src/test/java/com/isadorastrottmann/apicrudcustomer/contract/ContractTest.java
+++ src/test/java/com/isadorastrottmann/apicrudcustomer/contract/ContractTest.java
@@ -1,15 +1,15 @@
 package com.isadorastrottmann.apicrudcustomer.contract;
 
-import com.isadorastrottmann.apicrudcustomer.infra.security.TokenService;
-import com.isadorastrottmann.apicrudcustomer.repository.CustomerRepository;
-import org.junit.jupiter.api.Test;
-import org.junit.platform.commons.logging.Logger;
-import org.junit.platform.commons.logging.LoggerFactory;
-import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
+//import com.isadorastrottmann.apicrudcustomer.infra.security.TokenService;
+//import com.isadorastrottmann.apicrudcustomer.repository.CustomerRepository;
+//import org.junit.jupiter.api.Test;
+//import org.junit.platform.commons.logging.Logger;
+//import org.junit.platform.commons.logging.LoggerFactory;
+//import org.mockito.InjectMocks;
+//import org.springframework.beans.factory.annotation.Autowired;
 
-import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+//import static io.restassured.RestAssured.given;
+//import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class ContractTest {
 //    private final Logger log = LoggerFactory.getLogger(this.getClass());
@@ -21,7 +21,7 @@ public class ContractTest {
 //    private TokenService tokenService;
 //
 //    @Test
-//    void nemeusei() {
+//    void shouldCompareRsponseToExampleSchema() {
 //        var customer = customerRepository.findById("652fe2877bb4a6422a0b5e6c").get();
 //        log.info(() -> String.format("customer: "+customer));
 //        var token = tokenService.generateToken(customer);
